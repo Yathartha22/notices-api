e4eac6862b211c0ee6311fb56bc8c867e79abd74
Yathartha Joshi <yathartha32@gmail.com>
2017-11-12 23:38:49 +0530

    fixed #21 and some minor bugs

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import datetime
 from django.contrib.auth import get_user_model
+from accounts.models import Account
 
 class Api(models.Model):
 	User = get_user_model()
@@ -13,12 +14,7 @@ class Api(models.Model):
 	year = models.CharField(max_length=5, null=True)
 	branch = models.CharField(max_length=20, null=True)
 	choices = models.CharField(max_length=20, null=True)
-	user = models.OneToOneField(
-		User,
-		on_delete=models.CASCADE,
-		primary_key=False,
-		null=True
-	)
+	user = models.ForeignKey('accounts.Account', on_delete=models.CASCADE, null=True)
 
 	def __str__(self):
 		return '%s' %self.notice_name
