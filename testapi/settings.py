e76a603d1f544994b5f4b1b8ddae10b39f04d5ab
Yathartha Joshi <yathartha32@gmail.com>
2017-11-24 08:37:41 +0530

    improved uploading image capability

--- testapi/settings.py
+++ testapi/settings.py
@@ -166,3 +166,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ENV_PATH = os.path.abspath(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(ENV_PATH, 'uploaded_files/')
+MEDIA_URL = '/media/'
